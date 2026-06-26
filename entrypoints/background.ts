649ac55cd7cad40f1a29dfb41ac0fdbef391e181
VingeRay <vingeray@gmail.com>
2026-06-26 14:41:31 +0800

    feat: 安装后打开官网新手引导

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,10 +1,12 @@
+const OFFICIAL_SITE_URL = "https://vingeraycn.github.io/view-heic-browser-extension/?welcome=1#how-it-works"
+
 export default defineBackground(() => {
   console.log("🚀 View HEIC Extension Background Loaded")
 
-  // 扩展安装时显示欢迎信息
   browser.runtime.onInstalled.addListener((details) => {
     if (details.reason === "install") {
       console.log("🎉 View HEIC Extension 安装成功！")
+      browser.tabs.create({ url: OFFICIAL_SITE_URL })
     } else if (details.reason === "update") {
       console.log("🔄 View HEIC Extension 已更新到新版本")
     }
